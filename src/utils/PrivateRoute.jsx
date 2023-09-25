f785c89a9d016a8417d98d41504c7ed61c3aa5d8
chnk <119658399+chnk8802@users.noreply.github.com>
2023-09-25 16:36:01 +0530

    Done

--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,18 +1,41 @@
 import axios from "axios";
-import React from "react";
-import { Navigate, Route,Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, Route, Outlet } from "react-router-dom";
 import Cookies from "js-cookie";
 
-const isAuthenticated = async () => {
-    const token =Cookies.get("token");
-    if (token){
-        console.log(token);
-    }
-    const response = await axios.get("http://localhost:3000/users/validate", {withCredentials: true});
-    return response.data.isValidated;
-  };
-  const PrivateRoute = ({ element, ...rest }) => {
-    return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
-  };
+const PrivateRoute = ({ element, ...rest }) => {
+  const [authenticated, setAuthenticated] = useState(null); // Use state to track authentication status
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      const token = Cookies.get("token");
+      if (token) {
+        const _data = { token: token };
+        try {
+          const response = await axios.post(
+            "http://localhost:3000/users/validate",
+            _data,
+            { withCredentials: true }
+          );
+          console.log(response)
+          setAuthenticated(response.data.isValidated);
+        } catch (error) {
+          setAuthenticated(false);
+        }
+      } else {
+        setAuthenticated(false);
+      }
+    };
+
+    checkAuthentication();
+  }, []); // Call the function once when the component mounts
+
+  if (authenticated === null) {
+    // While authentication status is being determined, you can show a loading spinner or message
+    return <div>Loading...</div>;
+  }
+
+  return authenticated ? <Outlet /> : <Navigate to="/login" />;
+};
 
 export default PrivateRoute;
